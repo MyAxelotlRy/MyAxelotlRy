d7f703c77eaaae26d00d299af4ea1a8ba1839ad5
MyAxelotlRy <157206395+MyAxelotlRy@users.noreply.github.com>
2024-01-20 13:12:11 +0300

    MyAxelotRy.py

---
+++ MyAxelotRy.py
@@ -0,0 +1,3 @@
+import random
+gercekler = ["valorant en sevdiğim oyun","FF fire Flux yarı profesyonelim","Anime izliyorum","evcil hayvanım yok kardeşim var","Fenerliyim"]
+print(random.choice(gercekler))
